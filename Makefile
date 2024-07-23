a226db918246d921e5c4d03a45af9e9859537420
Yuriy Dubinin <yuradubinin100@gmail.com>
2024-07-23 16:46:44 +0300

    add

--- Makefile
+++
@@ -1,6 +0,0 @@
-build:
-	docker build -t inmemories-crm .
-run: 
-	docker run -d -p 3001:3001 -e PORT=3001 --rm --name inmemories-crm-container
-stop:
-	docker stop inmemories-crm-container
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'inMemories CRM',
+            title: 'Memorulink CRM',
             template: './src/index.html',
             favicon: './src/public/favicon.ico',
         }),
